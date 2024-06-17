dc5a5cb2fbdf3a1f5269dc061293b7e613055bd0
zijianhuang <zijianhuang2000@gmail.com>
2024-06-17 13:40:42 +1000

    disable parallel run of tests

--- TestReleaseAll.ps1
+++ TestReleaseAll.ps1
@@ -1,2 +1,2 @@
 ﻿cd $PSScriptRoot
-dotnet test --verbosity normal --configuration Release --no-build --filter FullyQualifiedName!~RemoteTests
+dotnet test -m:1 AuthEF.sln --verbosity normal --configuration Release --no-build --filter FullyQualifiedName!~RemoteTests
