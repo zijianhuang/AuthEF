54c66c89ba44348d73df74998af375247fd3949e
zijianhuang <zijianhuang2000@gmail.com>
2024-06-19 06:24:54 +1000

    Ready to release

--- AuthDbCreator/CreateBlankSqlite.ps1
+++ AuthDbCreator/CreateBlankSqlite.ps1
@@ -2,10 +2,11 @@
 # Error: Unhandled exception. System.TypeInitializationException: The type initializer for 'Microsoft.Data.Sqlite.SqliteConnection' threw an exception.
 # And it seems that the .NET runtime can only resolve through AuthDbCreator.deps.json rather than Fonlow.EntityFrameworkCore.Sqlite.deps.json, 
 # since Microsoft.Data.Sqlite.dll has no knowledge of Fonlow.EntityFrameworkCore.Sqlite.deps.json nor the runtimes folder.
+# 2 solutions:
+# 1. Copy all assemblies related to Sqlite, and copy the native e_sqlte3.* of specific OS from the runtimes folder.
+# 2. Use "dotnet publish -r WhatOS" to let dotnet choose the right one.
 cd $PSScriptRoot
 $netVersion = "net8.0"
-copy-item -Path ../Fonlow.EntityFrameworkCore.Sqlite/bin/Release/$netVersion/publish/* -Destination bin/Release/$netVersion/ -Filter *Sqlite*
-# Copy the Windows native dll of Sqlite
-copy-item -Path ../Fonlow.EntityFrameworkCore.Sqlite/bin/Release/$netVersion/publish/runtimes/win-x64/native/e_sqlite3.dll -Destination bin/Release/$netVersion/
+dotnet publish ../Fonlow.EntityFrameworkCore.Sqlite/Fonlow.EntityFrameworkCore.Sqlite.csproj  -r win-x64 --configuration Release --output bin/Release/$netVersion
 $connectionString = "Data Source=../Core3WebApi/DemoApp_Data/auth.db"
 bin/Release/net8.0/AuthDbCreator.exe Fonlow.EntityFrameworkCore.Sqlite $connectionString
