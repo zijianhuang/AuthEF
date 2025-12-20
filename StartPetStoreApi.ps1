b34b032d0cc3fcd6bb5bc4dd9dfef0ea5bc9a079
Z <zijianhuang2000@gmail.com>
2025-12-20 16:49:11 +1000

    published

--- StartPetStoreApi.ps1
+++ StartPetStoreApi.ps1
@@ -1,6 +1,6 @@
 ﻿#Launch WebApi Website and POST a request for generating client APIs
 Set-Location $PSScriptRoot
-$path = "$PSScriptRoot\PetWebApi\bin\Debug\net9.0"
+$path = "$PSScriptRoot\PetWebApi\bin\Debug\net10.0"
 $procArgs = @{
     FilePath         = "dotnet.exe"
     ArgumentList     = "$path\PetWebApi.dll"
