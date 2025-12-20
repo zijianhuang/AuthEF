f17e5ee2243a62da9864adfc92527dae8dea4352
Z <zijianhuang2000@gmail.com>
2025-12-20 16:57:09 +1000

    ps1

--- AuthDbCreator/CreateBlankMsSql.ps1
+++ AuthDbCreator/CreateBlankMsSql.ps1
@@ -1,9 +1,9 @@
 #requires -PSEdition Core
 Set-Location $PSScriptRoot
-$netVersion = "net9.0"
+$netVersion = "net10.0"
 $RuntimeId = ([System.Runtime.InteropServices.RuntimeInformation]::RuntimeIdentifier.ToString())
 $ExecutableExt = If ($IsWindows) {".exe"} Else {""}
 dotnet publish ../Fonlow.EntityFrameworkCore.MsSql/Fonlow.EntityFrameworkCore.MsSql.csproj -r $RuntimeId --configuration Release --output bin/Release/$netVersion
 $connectionString = "Server=localhost,1433;Database=DemoAppAuth_Test;User Id=sa;Password=Secured321*; TrustServerCertificate=True;"
 $CreatorExe = "AuthDbCreator" + $ExecutableExt;
-Invoke-Expression "&./bin/Release/net9.0/$CreatorExe Fonlow.EntityFrameworkCore.MsSql '$connectionString'"
+Invoke-Expression "&./bin/Release/$netVersion/$CreatorExe Fonlow.EntityFrameworkCore.MsSql '$connectionString'"
--- Fonlow.Auth.Models/GenerateTsClientModels.ps1
+++ Fonlow.Auth.Models/GenerateTsClientModels.ps1
@@ -1,2 +1,2 @@
 Set-Location $PSScriptRoot
-c:/green/poco2ts/Poco2TsCore.exe bin/Debug/net9.0/Fonlow.Auth.Models.dll ../AngularHeroes/src/clientapi/AuthModels.ts
+c:/green/poco2ts/Poco2TsCore.exe bin/Debug/net10.0/Fonlow.Auth.Models.dll ../AngularHeroes/src/clientapi/AuthModels.ts
